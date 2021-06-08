9ff8c44cfbf1b4f45314f75934b30fe098f2656b
George-North <52474045+Cynosure-North@users.noreply.github.com>
2021-06-08 18:56:48 +1200

    Fixed errors
    TODO:
    - Palm angle
    - Text on side
    - Thumb angle
    -Finger angle only returns approximate, reports straight as angled. Is that my code breaking?

--- Hands.py
+++ Hands.py
@@ -7,26 +7,18 @@ import numpy as np
 claws = True
 skeleton = True
 dots = True
-box = False
+box = True
 gesture_text = True
-angle_text = False
+joint_text = False
+finger_text = False
+text_on_side = False
+degrees = True
 
 # Setup stuff
 capture = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(False, 2, 0.8, 0.5)
 mpDraw = mp.solutions.drawing_utils
-
-
-
-def unit_vector(vector):
-    return vector / np.linalg.norm(vector)
-
-
-def angle_between(v1, v2):
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 def drawDebug(handLms, img):
 
@@ -40,12 +32,10 @@ def drawDebug(handLms, img):
             maxY = lm.y
         if lm.y < minY:
             minY = lm.y
-    minX, maxX, minY, maxY = int(
-        minX * w), int(maxX * w), int(minY * h), int(maxY * h)
+    minX, maxX, minY, maxY = int(minX * w), int(maxX * w), int(minY * h), int(maxY * h)
 
     if box:
-        img = cv2.rectangle(img, (minX - 7, minY - 7),
-                            (maxX + 7, maxY + 7), (0, 0, 255), 2)
+        img = cv2.rectangle(img, (minX - 7, minY - 7),(maxX + 7, maxY + 7), (0, 0, 255), 2)
 
     # landmarks
     if skeleton and dots:
@@ -56,64 +46,115 @@ def drawDebug(handLms, img):
         mpDraw.draw_landmarks(img, handLms)
 
     if claws:
-        for id in enumerate(handLms.landmark):
-            if id in (1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19):
-                start = (handLms.landmark.x, handLms.landmark[id].y)
-                end = vectorDiff2D(
-                    *start, handLms.landmark[id + 1].x, handLms.landmark[id + 1].y, 1)
-                img = cv2.line(img, (int(start[0] * w), int(start[1] * h)), (int(
-                    end[0] + start[0] * w), int(end[1] + start[1] * h)), 2)
-                print()
-                print(math.sqrt(end[0]**2 + end[1]**2))
+        for id in range(len(handLms.landmark)):
+            if id in (1,2,3, 5,6,7, 9,10,11, 13,14,15, 17,18,19):
+                start = (handLms.landmark[id].x, handLms.landmark[id].y, handLms.landmark[id].z)
+                end = vectorBetween(*start, handLms.landmark[id + 1].x, handLms.landmark[id + 1].y, handLms.landmark[id + 1].z, 0.2)
+                img = cv2.line(img, (int(start[0] * w), int(start[1] * h)), (int((end[0] + start[0]) * w), int((end[1] + start[1]) * h)), 2)
 
-    if angle_text:
-        for id, lm in enumerate(handLms.landmark):
+    if joint_text:
+        for id in range(len(handLms.landmark)):
             angle = jointAngle(id, handLms)
-            cv2.putText(img, angle, (int(lm.x * w), int(lm.y * h)),
-                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
+
+            if angle != None:
+                angle = "{:.2f}".format(jointAngle(id, handLms))
+            else:
+                angle = ""
+
+            if not text_on_side:
+                cv2.putText(img, angle, (int(handLms.landmark[id].x * w), int(handLms.landmark[id].y * h)),cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
+            else:
+                pass
+
+    if finger_text:
+        for id in (8,12,16,20):
+            angle = fingerAngle(id, handLms)
+
+            if angle != None:
+                angle = "{:.2f}".format(fingerAngle(id, handLms))
+            else:
+                angle = ""
+
+            if not text_on_side:
+                cv2.putText(img, angle, (int(handLms.landmark[id].x * w), int(handLms.landmark[id].y * h)),cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
+            else:
+                pass
+
+        #TODO: Thumb angle
 
     # text
     if gesture_text:
-        cv2.putText(img, gesture, (minX - 5, minY - 10),
-                    cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 0, 0))
-
+        gesture = recogniseGesture(handLms)
+        cv2.putText(img, gesture, (minX - 5, minY - 10),cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 0, 0))
 
 def recogniseGesture(handLms):
-    tiltDirection = 0
-    # TODO: It doesn't work if it's side on
+    #TODO: It doesn't work if it's side on
     gesture = ""
+
+    gesture += palmDirection(handLms)
+
+def palmDirection(handLms):
+    direction = ""
+    tiltDirection = 0
     for id, lm in enumerate(handLms.landmark):
         if id in (5, 9, 13, 17):
             tiltDirection += lm.z
 
     if tiltDirection < -0.3:
-        gesture += "forward "
+        direction += "forward "
     elif tiltDirection > 0.1:
-        gesture += "back "
+        direction += "back "
     else:
-        gesture += "upright "
+        direction += "upright "
 
-    return gesture
+    return direction
 
 
-def vectorDiff2D(x1, y1, x2, y2, scale=1):
+def vectorBetween(x1, y1, z1, x2, y2, z2, scale=1):
     deltaX = (x2 - x1)
     deltaY = (y2 - y1)
-    length = math.sqrt(deltaX ** 2 + deltaY ** 2)
-    deltaX = (deltaX / length) * scale
-    deltaY = (deltaY / length) * scale
-    return (deltaX, deltaY)
+    deltaZ = (z1 - z2)
+    length = math.sqrt(deltaX**2 + deltaY**2 + deltaZ**2)
+    x = (deltaX / length) * scale
+    y = (deltaY / length) * scale
+    z = (deltaZ / length) * scale
+    return (x, y, z)
 
+def nornalisedVector(x, y ,z=0):
+    length = math.sqrt(x**2 + y**2 + z**2)
+    return (x/length, y/length, z/length)
+
+
+def angle_between(x1,y1,z1, x2,y2,z2):
+    v1_u = nornalisedVector(x1,y1,z1)
+    v2_u = nornalisedVector(x2,y2,z2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 def jointAngle(id, handLms):
-    if id != 5 and id != 9 and id != 13 and id != 17:
+    if id not in (2,3, 5,6,7, 9,10,11, 13,14,15, 17,18,19):
         return
-    pass
-
+    if id in (1,5,9,13,17):
+        p1 = (handLms.landmark[0].x, handLms.landmark[0].y, handLms.landmark[0].z)
+    else:
+        p1 = (handLms.landmark[id-1].x, handLms.landmark[id-1].y, handLms.landmark[id-1].z)
+    p2 = (handLms.landmark[id].x, handLms.landmark[id].y, handLms.landmark[id].z)
+    p3 = (handLms.landmark[id+1].x, handLms.landmark[id+1].y, handLms.landmark[id+1].z)
+    v1 = vectorBetween(*p1, *p2)
+    v2 = vectorBetween(*p2, *p3)
+    angle = angle_between(*v1, *v2)
+    if degrees:
+        return angle * (180/math.pi)
+    else:
+        return angle
 
 def fingerAngle(id, handLms):
-    pass
-
+    if id not in ( 8,12,16,20):
+        return
+    else:
+        total = 0
+        for i in range(1,4):
+            total += jointAngle(id-i, handLms)
+        return total
 
 while True:
     try:
